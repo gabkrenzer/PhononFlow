885e9277c92f01dd3dff8da3ea2356f0d5f4aba8
Gabriel Krenzer <52633008+gabkrenzer@users.noreply.github.com>
2020-05-06 14:06:55 +0100

    Update run.sh

--- ModeMap/run.sh
+++ ModeMap/run.sh
@@ -5,7 +5,7 @@ do
 	cd $MODE
 	echo MODE $MODE
 
-	for NUM in {002..024} #update with the number of MPOSCAR
+	for NUM in {002..012} #update with the number of MPOSCAR
 	do
 		echo $NUM 
    		cd  $NUM
@@ -13,5 +13,12 @@ do
    		cd ../
    		sleep 1
 	done
-	cd ../
+	for NUM in {014..024} #update with the number of MPOSCAR
+	do
+		echo $NUM 
+   		cd  $NUM
+		qsub modemap.job
+   		cd ../
+   		sleep 1
+	done
 done
