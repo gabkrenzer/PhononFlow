ebc0fcb9e089b67e4a163ce586dd14a11357fc95
Gabriel Krenzer <52633008+gabkrenzer@users.noreply.github.com>
2020-05-06 13:57:37 +0100

    Update clean_ph.sh

--- phonopy/clean_ph.sh
+++ phonopy/clean_ph.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -l
 
-for NUM in {001..004}
+for NUM in {001..004} #update with the number of POSCAR-XXX generated
 do
 	cd $NUM
 	rm CHG* REPORT 
 	rm WAVECAR CONTCAR
 	rm phonon.e* phonon.o* phonon.po* phonon.pe*
+	#you can add up files if you want
 	cd ../
 done
 
