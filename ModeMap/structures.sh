d79ec860b10a0fb9b663e0c5a723d10ef7fa59fa
Gabriel Krenzer <52633008+gabkrenzer@users.noreply.github.com>
2020-05-07 12:32:44 +0100

    Update structures.sh

--- ModeMap/structures.sh
+++ ModeMap/structures.sh
@@ -8,8 +8,8 @@ do
 	cp POTCAR KPOINTS INCAR modemap.job $NUM
 	cp clean_mm.sh $NUM
  	cd $NUM
-	#adapt tje following line to your system
-	python ModeMap.py -c POSCAR --dim="2 2 2" --mode="0 0 0 $NUM" --q_range="-1 1 0.1" --supercell="1 1 1"
+	#update the following line to your system
+	python ModeMap.py -c POSCAR --dim="2 2 1" --mode="0.333333 0.333333 0 $NUM" --q_range="-6 6 0.5" --supercell="3 3 1"
 	tar -xf ModeMap.tar.gz
 	cd ../
 	sleep 1
