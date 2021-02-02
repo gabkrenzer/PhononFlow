f830ef691ddc346331828be1eccf9b698188bb76
Gabriel Krenzer <52633008+gabkrenzer@users.noreply.github.com>
2021-02-02 11:11:03 +0100

    Update energies.sh

--- ModeMap/energies.sh
+++ ModeMap/energies.sh
@@ -1,11 +1,11 @@
 #/bin/bash -l
 
-for MODE in 1 #update with the number of modes that are of interest
+for MODE in X Y Z #update with the number of modes that are of interest
 do
 	cd $MODE
 	echo $MODE
 	python ExtractTotalEnergies.py
-	for NUM in {001..021} #update with the number of MPOSCAR generated
+	for NUM in {001..XXX} #update with the number of MPOSCAR generated
 	do	
 		cd $NUM
 		./clean_mm.sh
