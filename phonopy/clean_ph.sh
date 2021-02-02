6d363d9847e06282e57800fb5c9c82aa96a64b4e
Gabriel Krenzer <52633008+gabkrenzer@users.noreply.github.com>
2021-02-02 10:42:58 +0100

    Update clean_ph.sh

--- phonopy/clean_ph.sh
+++ phonopy/clean_ph.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -l
 
-for NUM in {001..004} #update with the number of POSCAR-XXX generated
+for NUM in {001..XXX} #update with the number of POSCAR-XXX generated
 do
-	cd $NUM
-	rm CHG* REPORT 
-	rm WAVECAR CONTCAR
-	rm phonon.e* phonon.o* phonon.po* phonon.pe*
-	#you can add up files if you want
-	cd ../
+   echo $NUM
+   mkdir $NUM
+   cp -r INCAR POTCAR KPOINTS ./$NUM/
+   cp POSCAR-$NUM $NUM/POSCAR
+   cp phonon.job $NUM/
+   cd  $NUM
+   qsub phonon.job #it is recommended to test convergence/paralellisation before submitting all jobs
+   cd ../
+   sleep 1
 done
 
 
