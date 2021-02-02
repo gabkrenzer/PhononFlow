8ea7245bfe01b6f3de9c74b28cd49611950727f7
Gabriel Krenzer <52633008+gabkrenzer@users.noreply.github.com>
2021-02-02 10:43:35 +0100

    Update fd.sh

--- phonopy/fd.sh
+++ phonopy/fd.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -l
 
-for NUM in {001..003} #update with the number of POSCAR-XXX generated
+for NUM in {001..XXX} #update with the number of POSCAR-XXX generated
 do
-   echo $NUM  
-   mkdir $NUM 
-   cp -r INCAR POTCAR ./$NUM/
-   cp -r KPOINTS ./$NUM/
+   echo $NUM
+   mkdir $NUM
+   cp -r INCAR POTCAR KPOINTS ./$NUM/
    cp POSCAR-$NUM $NUM/POSCAR
    cp phonon.job $NUM/
    cd  $NUM
-   qsub phonon.job
+   qsub phonon.job #it is recommended to test convergence/paralellisation before submitting all jobs
    cd ../
    sleep 1
 done
