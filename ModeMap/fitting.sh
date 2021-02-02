875fc8ca9cd14bde66d8d8414935ace54cfdbb7b
Gabriel Krenzer <52633008+gabkrenzer@users.noreply.github.com>
2021-02-02 11:11:39 +0100

    Update fitting.sh

--- ModeMap/fitting.sh
+++ ModeMap/fitting.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 
-for MODE in 1 #update with the number of modes of interest
+for MODE in X Y Z #update with the number of modes of interest
 do
 	echo $MODE
 	cp ModeMap_PolyFit.py $MODE
 	cd $MODE
-	python ModeMap_PolyFit.py --degree=6 #update for your needs
+	python ModeMap_PolyFit.py --degree=N #update N to your needs
 	cd ../
 	sleep 1 
 done
